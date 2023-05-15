4962af0e1f161e400115598efa08227e9ea682d1
Marq W <marquisewest86@duck.com>
2023-05-15 11:23:07 -0500

    updated file paths

---
+++ server.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const exp = require('constants');
+// const helpers = require('./utils/helpers');
+
+const app = express();
+const routes = require('./controllers');
+// Express middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+const PORT = process.env.PORT || 3001;
+const io = require('socket.io')(PORT);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
+  
+
+io.on('connection', socket => {
+    console.log(socket.id)
+})
+
+// module.exports = sequelize;
