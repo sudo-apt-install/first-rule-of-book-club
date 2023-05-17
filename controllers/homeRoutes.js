75c8fa4fb735be1b7667d91aca2a43ce41e73231
Emma Carr <lorenagracex@gmail.com>
2023-05-17 13:04:13 -0500

    api routes, renders images, connected search bar button, and established new node package

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
 // const { Project, User } = require('../models');
 const withAuth = require('../utils/auth');
+// node package to fetch API
 const axios = require('axios');
-require("dotenv").config();
+// node package to clean api render
+const sanitizeHtml = require("sanitize-html");
+const axios = require('axios');
 const apiKey = process.env.API_KEY;
+require("dotenv").config();
+
 
 router.get('/login', (req, res) => {
   // if user logged in redirect them to the profile page
@@ -29,22 +34,65 @@ router.get('join/', (req, res) => {
 // API routes
 // keyword is genre & then include 0 to start index
 router.get("/search-results/:keyword/:startindex", (req, res) => {
+   axios
+     .get("https://www.googleapis.com/books/v1/volumes", {
+       params: {
+         q: req.params.keyword,
+         maxResults: 40,
+         startIndex: parseInt(req.params.startindex),
+         key: apiKey,
+       },
+     })
+     .then(({ data }) => {
+       const results = data.items.map((item) => {
+         const book = item.volumeInfo;
+         const coverPhoto = book.imageLinks?.thumbnail || "";
+         const authors = book.authors || [];
+         const bookId = item.id || "";
+         return {
+           title: book.title || "Unknown Title",
+           coverPhoto,
+           authors: authors.join(", ") || "Unknown Author",
+           bookId,
+         };
+       });
+
+       res.render("search-results", { results, total: data.totalItems });
+     })
+     .catch((err) => res.status(err.response.status).send("Error"));
+});
+
+// gets book info from the api and renders on the book page
+router.get("/book/:bookId", (req, res) => {
+  const bookId = req.params.bookId;
+
   axios
-    .get("https://www.googleapis.com/books/v1/volumes", {
+    .get(`https://www.googleapis.com/books/v1/volumes/${bookId}`, {
       params: {
-        q: req.params.keyword,
-        maxResults: 40,
-        startIndex: parseInt(req.params.startindex),
         key: apiKey,
       },
     })
     .then(({ data }) => {
-      res.render("search-results", {
-        data: data.items,
-        total: data.totalItems,
-      });
+      const book = {
+        title: data.volumeInfo.title || "Unknown Title",
+        authors: data.volumeInfo.authors || ["Unknown Author"],
+        publishedDate: data.volumeInfo.publishedDate || "Unknown Publish Date",
+        description:
+          sanitizeHtml(data.volumeInfo.description, { allowedTags: [] }) ||
+          "No description available.",
+        categories: data.volumeInfo.categories || ["Uncategorized"],
+        pageCount: data.volumeInfo.pageCount || 0,
+        coverPhoto:
+          data.volumeInfo.imageLinks?.thumbnail ||
+          "https://example.com/default-cover.jpg",
+      };
+
+      res.render("book-page", { book });
     })
-    .catch((err) => res.status(err.response.status).send("Error"));
+    .catch((err) => {
+      console.error("Error fetching book details:", err);
+      res.status(500).send("Error fetching book details");
+    });
 });
 
 module.exports = router;
