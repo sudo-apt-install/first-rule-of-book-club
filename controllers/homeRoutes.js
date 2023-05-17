c82eb61e9f2db525250314f80e3d52a8f92927dd
Emma Carr <lorenagracex@gmail.com>
2023-05-17 11:42:28 -0500

    api works & new search results page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,20 +1,9 @@
 const router = require('express').Router();
 // const { Project, User } = require('../models');
 const withAuth = require('../utils/auth');
-// const axios = require('axios');
-
-// basic api call
-// router.get("/", async (req, res) => {
-//   try {
-//     const url = api url info
-
-//     const { data } = axios.get(url)
-
-//     res.render("homepage", data);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+const axios = require('axios');
+require("dotenv").config();
+const apiKey = process.env.API_KEY;
 
 router.get('/login', (req, res) => {
   // if user logged in redirect them to the profile page
@@ -36,85 +25,26 @@ router.get('join/', (req, res) => {
   res.render("join");
 })
 
-router.get("/", async (req, res) => {
-  res.render("homepage");
-});router
 
-router.get('/', async (req, res) => {
-  try {
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      projects, 
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/project/:id', async (req, res) => {
-  try {
-    const projectData = await Project.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    const project = projectData.get({ plain: true });
-
-    res.render('project', {
-      ...project,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
-
-  res.render('login');
+// API routes
+// keyword is genre & then include 0 to start index
+router.get("/search-results/:keyword/:startindex", (req, res) => {
+  axios
+    .get("https://www.googleapis.com/books/v1/volumes", {
+      params: {
+        q: req.params.keyword,
+        maxResults: 40,
+        startIndex: parseInt(req.params.startindex),
+        key: apiKey,
+      },
+    })
+    .then(({ data }) => {
+      res.render("search-results", {
+        data: data.items,
+        total: data.totalItems,
+      });
+    })
+    .catch((err) => res.status(err.response.status).send("Error"));
 });
 
 module.exports = router;
