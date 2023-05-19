8c178831f7334b98f3235195a5528302ebb6788d
Emma Carr <lorenagracex@gmail.com>
2023-05-18 21:40:14 -0500

    just trying things out fr

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,90 +1,150 @@
-const router = require('express').Router();
-const { User } = require('../../models/User');
+const router = require("express").Router();
+const { User } = require("../../models/User");
 
 // router.get("/api/users/login", (req, res) => {
 //     res.render("login");
 //   });
 
-router.post('/', async (req, res) => {
-    try {
-        const userData = await User.create(req.body);
+// router.post("/", async (req, res) => {
+//   try {
+//     console.log(req.body);
+//     const userData = await User.create(req.body);
 
-        req.session.save(() => {
-            req.session.user_id = userData.id;
-            req.session.logged_in = true;
+//     req.session.save(() => {
+//       req.session.username = userData.username;
+//       req.session.user_id = userData.id;
+//       req.session.email = userData.email;
+//       req.session.logged_in = true;
+//     });
 
-            res.status(200).json(userData);
-        });
-    } catch (err) {
-        res.status(400).json(err);
+//     res.status(200).json({ user: userData, message: "welcome!" });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(400).json(err);
+//   }
+// });
+
+// router.post("/", async (req, res) => {
+//   try {
+//     const userData = await User.findOne({ where: { username: req.body.username } });
+
+//     if (!userData) {
+//       res
+//         .status(400)
+//         .json({ message: "Incorrect email or password, please try again" });
+//       return;
+//     }
+
+//     const validPassword = await userData.checkPassword(req.body.password);
+
+//     if (!validPassword) {
+//       res
+//         .status(400)
+//         .json({ message: "Incorrect email or password, please try again" });
+//       return;
+//     }
+
+//     req.session.save(() => {
+//       req.session.username = userData.username;
+//       req.session.user_id = userData.id;
+//       req.session.logged_in = true;
+//       res.json({ user: userData.name, message: "You are now logged in!" });
+//     });
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
+
+
+
+// router.get("/profile/:userId", (req, res) => {
+//   const userId = req.params.userId;
+
+//   // Retrieve the bookshelf data for the specific user from your database or storage
+
+//   Bookshelf.findAll({
+//     where: { user_id: userId },
+//     include: [{ model: Book }],
+//   })
+//     .then((bookshelfData) => {
+//       const bookshelf = bookshelfData.map((shelf) => ({
+//         name: shelf.name,
+//         books: shelf.Books.map((book) => book.title),
+//       }));
+
+//       res.render("user-profile", { bookshelf });
+//     })
+//     .catch((err) => {
+//       console.error("Error fetching user bookshelf:", err);
+//       res.status(500).send("Error fetching user bookshelf");
+//     });
+// });
+
+// router.get("/profile", async (req, res) => {
+//   try {
+//     const userId = req.session.userId;
+//     // Retrieve the user information from the database
+//     const user = await User.findByPk(userId);
+
+//     console.log(user); // Check the user data in the console
+//     res.render("profile", { user });
+
+//     if (!user) {
+//       // Handle the case where the user is not found
+//       // You can redirect to an error page or show an appropriate message
+//       return res.redirect("/error");
+//     }
+//     // Render the profile page template and pass the user's information as data
+//     res.render("profile", { user });
+//   } catch (error) {
+//     console.error("An error occurred:", error);
+//     // Handle the error appropriately, e.g., redirect to an error page
+//     res.redirect("/error");
+//   }
+// });
+
+// router.post("/logout", (req, res) => {
+//   if (req.session.logged_in) {
+//     req.session.destroy(() => {
+//       res.status(204).end();
+//     });
+//   } else {
+//     res.status(404).end();
+//   }
+// });
+
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { username: req.body.username } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect username or password, please try again" });
+      return;
     }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect username or password, please try again" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.username = userData.username;
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res.json({ user: userData.name, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.post('/login', async (req, res) => {
-    try {
-        const userData = await User.findOne({ where: { email: req.body.email } });
 
-        if (!userData) {
-            res
-                .status(400)
-                .json({ message: 'Incorrect email or password, please try again' });
-            return;
-        }
 
-        const validPassword = await userData.checkPassword(req.body.password);
-
-        if (!validPassword) {
-            res
-                .status(400)
-                .json({ message: 'Incorrect email or password, please try again' });
-            return;
-        }
-
-        req.session.save(() => {
-            req.session.user_id = userData.id;
-            req.session.logged_in = true;
-
-            res.json({ user: userData, message: 'You are now logged in!' });
-        });
-
-    } catch (err) {
-        res.status(400).json(err);
-    }
-});
-
-router.get("/profile/:userId", (req, res) => {
-    const userId = req.params.userId;
-  
-    // Retrieve the bookshelf data for the specific user from your database or storage
-  
-    Bookshelf.findAll({
-      where: { user_id: userId },
-      include: [{ model: Book }],
-    })
-      .then((bookshelfData) => {
-        const bookshelf = bookshelfData.map((shelf) => ({
-          name: shelf.name,
-          books: shelf.Books.map((book) => book.title),
-        }));
-  
-        res.render("user-profile", { bookshelf });
-      })
-      .catch((err) => {
-        console.error("Error fetching user bookshelf:", err);
-        res.status(500).send("Error fetching user bookshelf");
-      });
-  });
-
-router.post('/logout', (req, res) => {
-    if (req.session.logged_in) {
-        req.session.destroy(() => {
-            res.status(204).end();
-        });
-    } else {
-        res.status(404).end();
-    }
-});
 
 module.exports = router;
-
-
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,16 +1,63 @@
 const router = require('express').Router();
-// const { Project, User } = require('../models');
+const { User } = require("../models/User");
 const withAuth = require('../utils/auth');
+const sequelize = require('sequelize'); 
 // node package to fetch API
 const axios = require('axios');
 // node package to clean api render
 const sanitizeHtml = require('sanitize-html');
-// const Bookshelf = require("../models/Bookshelf");
 const apiKey = process.env.API_KEY;
 require("dotenv").config();
 
+// router.post("/", async (req, res) => {
+//   try {
+//     const userData = await User.findOne({ where: { username: req.body.username } });
+
+//     if (!userData) {
+//       res
+//         .status(400)
+//         .json({ message: "Incorrect username or password, please try again" });
+//       return;
+//     }
+
+//     const validPassword = await userData.checkPassword(req.body.password);
+
+//     if (!validPassword) {
+//       res
+//         .status(400)
+//         .json({ message: "Incorrect username or password, please try again" });
+//       return;
+//     }
+
+//     req.session.save(() => {
+//       req.session.username = userData.username;
+//       req.session.user_id = userData.id;
+//       req.session.logged_in = true;
+//       res.json({ user: userData.name, message: "You are now logged in!" });
+//     });
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
+
+
 
 // Prevent non logged in users from viewing the homepage
+
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/profile", {
+      loggedUser: req.session.username,
+      logged_in: req.session.logged_in,
+    });
+    return;
+  }
+  res.render("login");
+});
+
+
+
 router.get('/search-results', withAuth, async (req, res) => {
   try {
     const userData = await User.findAll({
@@ -20,7 +67,7 @@ router.get('/search-results', withAuth, async (req, res) => {
 
     const users = userData.map((project) => project.get({ plain: true }));
 
-    res.render('search-results', {
+    res.render('homepage', {
       users,
       // Pass the logged in flag to the template
       logged_in: req.session.logged_in,
@@ -31,57 +78,57 @@ router.get('/search-results', withAuth, async (req, res) => {
 });
 
 
-router.get('/login', (req, res) => {
-  // if user logged in redirect them to the profile page
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
-// else render the login page
-  res.render('login');
-});
+// router.get('/login', (req, res) => {
+//   // if user logged in redirect them to the profile page
+//   if (req.session.logged_in) {
+//     res.redirect('/profile');
+//     return;
+//   }
+// // else render the login page
+//   res.render('login');
+// });
 
-router.get('/signup', (req, res) => {
-  res.render('signup');
-});
+// router.get('/signup', (req, res) => {
+//   res.render('signup');
+// });
 
-router.get('join/', (req, res) => {
-  // if user logged in redirect them to profile
-  if (req.session.logged_in) {
-    res.redirect("/profile");
-    return;
-  }
-  // else, render the join page
-  res.render("join");
-})
+// router.get('join/', (req, res) => {
+//   // if user logged in redirect them to profile
+//   if (req.session.logged_in) {
+//     res.redirect("/profile");
+//     return;
+//   }
+//   // else, render the join page
+//   res.render("join");
+// })
 
-router.get("/", (req, res) => {
-  res.render("homepage");
-});
+// router.get("/", (req, res) => {
+//   res.render("homepage");
+// });
 
-router.get("/login", (req, res) => {
-  res.render("login");
-});
+// router.get("/login", (req, res) => {
+//   res.render("login");
+// });
 
-router.get("/profile", (req, res) => {
-  res.render("profile");
-});
+// router.get("/profile", (req, res) => {
+//   res.render("profile");
+// });
 
-router.get("/join", (req, res) => {
-  res.render("join");
-});
+// router.get("/join", (req, res) => {
+//   res.render("join");
+// });
 
-router.get("/rules", (req, res) => {
-  res.render("rules");
-});
+// router.get("/rules", (req, res) => {
+//   res.render("rules");
+// });
 
-router.get("/search-results", (req, res) => {
-  res.render("search-results");
-});
+// router.get("/search-results", (req, res) => {
+//   res.render("search-results");
+// });
 
-router.get("/aboutus", (req, res) => {
-  res.render("aboutus");
-});
+// router.get("/aboutus", (req, res) => {
+//   res.render("aboutus");
+// });
 
 
 
@@ -118,28 +165,6 @@ router.get("/book/:bookId", (req, res) => {
     });
 });
 
-// router.get("/profile/:userId", (req, res) => {
-//   const userId = req.params.userId;
-
-//   // Retrieve the bookshelf data for the specific user from your database or storage
-
-//   Bookshelf.findAll({
-//     where: { user_id: userId },
-//     include: [{ model: Book }],
-//   })
-//     .then((bookshelfData) => {
-//       const bookshelf = bookshelfData.map((shelf) => ({
-//         name: shelf.name,
-//         books: shelf.Books.map((book) => book.title),
-//       }));
-
-//       res.render("user-profile", { bookshelf });
-//     })
-//     .catch((err) => {
-//       console.error("Error fetching user bookshelf:", err);
-//       res.status(500).send("Error fetching user bookshelf");
-//     });
-// });
 
 router.get("/search-results/:keyword", (req, res) => {
  const startIndex =  req.query.startindex ?  parseInt(req.query.startindex) :  0
